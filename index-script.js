37a5a80ce3f086a96529b49ca1cf02d0fa65fe24
vy4islitel <145319271+vy4islitel@users.noreply.github.com>
2023-10-02 02:09:27 +0300

    Add files via upload

--- index-script.js
+++ index-script.js
@@ -2,13 +2,14 @@ let date = document.getElementById('inputDate')
 let car = document.getElementById('inputCar')
 let company = document.getElementById('inputCompany')
 let guests = document.getElementById('inputGuests')
-        
+
 const submitButton = document.getElementById('submitButton')
 const result = document.getElementById('resultBox')
 const copyButton = document.getElementById('copyButton')
 const copyAnswerBox = document.getElementById('copyAnswerBox')
 const copyAnswerButton = document.getElementById('copyAnswerButton')
 const contactsBox = document.getElementById('contactsBox')
+const routeTTbox = document.getElementById('routeTTbox')
 
 // Кнопки с выбором - пропуск, отбивки, контакты и маршрутизация
 const passSelectButton = document.getElementById('passSelectBtn')
@@ -17,6 +18,9 @@ const contactsSelectButton = document.getElementById('contactsSelectBtn')
 const routeTTSelectButton = document.getElementById('routeTTSelectBtn')
 const goBackBox = document.getElementById('goBackBox')
 const goBackBox2 = document.getElementById('goBackBox2')
+const aboutGroupsShBtn = document.getElementById('aboutGroupsShBtn')
+const routeTTtableShBtn = document.getElementById('routeTTtableShBtn')
+const routeTTexamplesBtn = document.getElementById('routeTTexamplesBtn')
 
 // Кнопки с выбором - закрыть, открыть и т.п.
 const createdTTButton = document.getElementById('createdTTBtn')
@@ -29,49 +33,50 @@ const closedTTButton = document.getElementById('closedTTBtn')
 const themeButton = document.getElementById('themeButton')
 const jsBody = document.getElementById('jsBody')
 
-passSelectButton.onclick = function() {
+passSelectButton.onclick = function () {
     document.getElementById('passBox').style = "display: block;"
     document.getElementById('welcomeBox').style = "display:none;"
 
-    goBackBox.innerHTML = 
-    `
+    goBackBox.innerHTML =
+        `
     <button class="choose_button" id="goBackBtn">НА ГЛАВНУЮ</button>
     `
 
     const goBackButton = document.getElementById('goBackBtn')
     document.getElementById('goBackBtn').style = "display: flex;"
 
-    goBackButton.onclick = function() {
+    goBackButton.onclick = function () {
         document.getElementById('passBox').style = "display: none;"
         document.getElementById('welcomeBox').style = "display: block;"
         document.getElementById('goBackBtn').style = "display: none;"
     }
 }
 
-routeTTSelectButton.onclick = function() {
-    alert('Упс! Страница ещё в разработке =(')
+routeTTSelectButton.onclick = function () {
+    document.getElementById('routeTTbox').style = 'display:block;'
+    document.getElementById('welcomeBox').style = 'display:none;'
 }
 
-answersSelectButton.onclick = function() {
+answersSelectButton.onclick = function () {
     document.getElementById('answersBox').style = "display: block;"
     document.getElementById('welcomeBox').style = "display: none;"
 
     goBackBox2.innerHTML =
-    `
+        `
     <button class="choose_button" id="goBackBtn2">НА ГЛАВНУЮ</button>
     `
 
     const goBackButton2 = document.getElementById('goBackBtn2')
     document.getElementById('goBackBtn2').style = "display: flex;"
 
-    createdTTButton.onclick = function() {
+    createdTTButton.onclick = function () {
         let ticketNumber = document.getElementById('inputTicketNumber')
         let createdTTtext = 'Здравствуйте! Дата-центр O2XYGEN свидетельствует Вам своё уважение и информирует о том, что по Вашему запросу была открыта заявка № ' + ticketNumber.value
         console.log(createdTTtext)
         let createdTTAnswerText = document.getElementById('createdAnswerTextBox')
 
         createdTTAnswerText.innerHTML =
-        `
+            `
         <textarea class="text_for_copy2" cols="90" rows="2" id="textForCopyCreatedTT">${createdTTtext}</textarea>
         `
 
@@ -80,14 +85,14 @@ answersSelectButton.onclick = function() {
         document.execCommand('copy')
     }
 
-    moreInfoTTButton.onclick = function() {
+    moreInfoTTButton.onclick = function () {
         let ticketNumber = document.getElementById('inputTicketNumber')
         let moreInfoTTtext = 'Дата-центр O2XYGEN свидетельствует Вам своё уважение и информирует о том, что по заявке № ' + ticketNumber.value + ' необходима дополнительная информация:'
         console.log(moreInfoTTtext)
         let moreInfoTTAnswerText = document.getElementById('createdAnswerTextBox')
 
         moreInfoTTAnswerText.innerHTML =
-        `
+            `
         <textarea class="text_for_copy2" cols="90" rows="2" id="textForCopyCreatedTT">${moreInfoTTtext}</textarea>
         `
 
@@ -96,14 +101,14 @@ answersSelectButton.onclick = function() {
         document.execCommand('copy')
     }
 
-    solutionTTButton.onclick = function() {
+    solutionTTButton.onclick = function () {
         let ticketNumber = document.getElementById('inputTicketNumber')
         let solutionTTtext = 'Дата-центр O2XYGEN свидетельствует Вам своё уважение и информирует о том, что по заявке № ' + ticketNumber.value + ' появилось решение:'
         console.log(solutionTTtext)
         let solutionTTAnswerText = document.getElementById('createdAnswerTextBox')
 
         solutionTTAnswerText.innerHTML =
-        `
+            `
         <textarea class="text_for_copy2" cols="90" rows="2" id="textForCopyCreatedTT">${solutionTTtext}</textarea>
         `
 
@@ -112,14 +117,14 @@ answersSelectButton.onclick = function() {
         document.execCommand('copy')
     }
 
-    canIcloseTTButton.onclick = function() {
+    canIcloseTTButton.onclick = function () {
         let ticketNumber = document.getElementById('inputTicketNumber')
         let canIcloseTTtext = 'Здравствуйте! Уточните, пожалуйста: можем ли мы закрыть заявку № ' + ticketNumber.value + ' в случае, если по ней у Вас не осталось больше вопросов?'
         console.log(canIcloseTTtext)
         let canIcloseTTAnswerText = document.getElementById('createdAnswerTextBox')
 
         canIcloseTTAnswerText.innerHTML =
-        `
+            `
         <textarea class="text_for_copy2" cols="90" rows="2" id="textForCopyCreatedTT">${canIcloseTTtext}</textarea>
         `
 
@@ -128,14 +133,14 @@ answersSelectButton.onclick = function() {
         document.execCommand('copy')
     }
 
-    closedTTButton.onclick = function() {
+    closedTTButton.onclick = function () {
         let ticketNumber = document.getElementById('inputTicketNumber')
         let closedTTtext = 'Здравствуйте! Дата-центр O2XYGEN свидетельствует Вам своё уважение и информирует о том, что заявка № ' + ticketNumber.value + ' закрыта. Просим Вас не отвечать на данное письмо кроме случая, если по данной заявке у Вас остались невыясненные вопросы. Рады помочь Вам!'
         console.log(closedTTtext)
         let closedTTAnswerText = document.getElementById('createdAnswerTextBox')
 
-        closedTTAnswerText.innerHTML = 
-        `
+        closedTTAnswerText.innerHTML =
+            `
         <textarea class="text_for_copy2" cols="90" rows="3" id="textForCopyCreatedTT">${closedTTtext}</textarea>
         `
 
@@ -144,7 +149,7 @@ answersSelectButton.onclick = function() {
         document.execCommand('copy')
     }
 
-    goBackButton2.onclick = function() {
+    goBackButton2.onclick = function () {
         document.getElementById('answersBox').style = "display: none;"
         document.getElementById('welcomeBox').style = "display: block;"
         document.getElementById('goBackBtn2').style = "display: none;"
@@ -152,12 +157,12 @@ answersSelectButton.onclick = function() {
 }
 
 
-submitButton.onclick = function() {
+submitButton.onclick = function () {
     const compared = date.value + ' // ' + company.value + ' // ' + guests.value + ' // ' + car.value
     console.log(compared)
 
-    result.innerHTML = 
-    `
+    result.innerHTML =
+        `
     <textarea class="text_for_copy" id="textForCopy">${compared}</textarea>
     `
 
@@ -168,27 +173,52 @@ submitButton.onclick = function() {
     alert('Попуск скопирован! Теперь просто вставь его в таск! :3')
 }
 
-copyAnswerButton.onclick = function() {
+copyAnswerButton.onclick = function () {
 
 }
 
-contactsSelectButton.onclick = function() {
+contactsSelectButton.onclick = function () {
     document.getElementById('contactsBox').style = "display: block;"
     let goBackBox3 = document.getElementById('goBackBox3')
-    goBackBox3.innerHTML = 
-    `
+    goBackBox3.innerHTML =
+        `
     <button class="choose_button" id="goBackBtn3">НА ГЛАВНУЮ</button>
     `
     document.getElementById('goBackBox3').style = "display: flex;"
     document.getElementById('welcomeBox').style = "display: none;"
     const goBackButton3 = document.getElementById('goBackBtn3')
 
-    goBackButton3.onclick = function() {
+    goBackButton3.onclick = function () {
         document.getElementById('welcomeBox').style = "display: block;"
         document.getElementById('goBackBox').style = "display: none;"
         document.getElementById('contactsBox').style = "display: none;"
     }
 
+}
+
+aboutGroupsShBtn.onclick = function() {
+    document.getElementById('routeTTboxButtonsBar').style = 'display: none;'
+    document.getElementById('routeTTboxWelcome').style = 'display: none;'
+    document.getElementById('routeTTboxAboutGroups').style = 'display: block;'
+    document.getElementById('routeTTgoBackBtntoList').style = 'display: flex;'
+    document.getElementById('routeTTgoBackBtntoList').innerHTML = `<button id="rtTTgbBtnToList" class="choose_button">НАЗАД</button`
+    let rtTTgbBtnToList = document.getElementById('rtTTgbBtnToList')
+
+    rtTTgbBtnToList.onclick = function() {
+        document.getElementById('routeTTboxButtonsBar').style = 'display: flex;'
+        document.getElementById('routeTTboxWelcome').style = 'display: block;'
+        document.getElementById('routeTTboxAboutGroups').style = 'display: none;'
+        document.getElementById('routeTTgoBackBtntoList').style = 'display: none;'
+    }
+
+}
+
+routeTTtableShBtn.onclick = function() {
+    document.getElementById('routeTTtable').style = 'display: block;'
+}
+
+routeTTexamplesBtn.onclick = function() {
+    alert('Упс! Страница ещё в разработке!')
 }
 
 let now = new Date()
---
+++ plannedworkd-script.js
@@ -0,0 +1,88 @@
+let selectService = document.getElementById('selectService')
+
+const boxVOLS = document.getElementById('VOLS')
+const boxLINK = document.getElementById('LINK')
+const boxINTERNET = document.getElementById('INTERNET')
+
+selectService.onchange = function () {
+    let serviceOption = selectService.value
+    console.log(serviceOption)
+
+    if (serviceOption === 'ВОЛС')
+        document.getElementById('LINK').style = 'display: none;', document.getElementById('INTERNET').style = 'display: none;', document.getElementById('VOLS').style = 'display: block;', function funcVOLS() {
+            let VOLSa
+            let VOLSb
+
+            let inputVOLSa = document.getElementById('inputVOLSa')
+            let inputVOLSb = document.getElementById('inputVOLSb')
+
+            VOLSa = 'Адрес точки А:  ' + inputVOLSa.value + '    '
+            VOLSb = 'Адрес точки Б:  ' + inputVOLSb.value
+
+            console.log(VOLSa, VOLSb)
+        }
+    else if (serviceOption === 'Канал Связи')
+        document.getElementById('LINK').style = 'display: block;', document.getElementById('INTERNET').style = 'display: none;', document.getElementById('VOLS').style = 'display: none;', function () {
+            let LINKa
+            let LINKb
+
+            let inputLINKa = document.getElementById('inputLINKa')
+            let inputLINKb = document.getElementById('inputLINKb')
+
+            LINKa = 'Адрес точки А:  ' + inputLINKa.value + '    '
+            LINKb = 'Адрес точки Б:  ' + inputLINKb.value + '    '
+
+            console.log(LINKa, LINKb)
+        }
+    else if (serviceOption === 'Интернет')
+        document.getElementById('LINK').style = 'display: none;', document.getElementById('INTERNET').style = 'display: none;', document.getElementById('VOLS').style = 'display: none;', function funcInternet() {
+            let INTERNET
+
+            let inputINTERNET = document.getElementById('inputINTERNET')
+
+            INTERNET = 'Наименование договора:  ' + inputINTERNET.value
+
+            console.log(INTERNET)
+        }, alert('С Интернетом пока всё туго - в разработке :3')
+    else if (serviceOption === 'Выбрать...')
+        document.getElementById('LINK').style = 'display: none;', document.getElementById('INTERNET').style = 'display: none;', document.getElementById('VOLS').style = 'display: none;'
+}
+
+const submitFormButton = document.getElementById('submitButton')
+
+submitFormButton.onclick = function Saa() {
+    let inputTTNumber = document.getElementById('inputTTnumber')
+    let ticketNumber = inputTTNumber.value
+    console.log(ticketNumber)
+
+    let worksInterval
+    let inputDateA = document.getElementById('dateA')
+    let inputTimeA = document.getElementById('timeA')
+    let inputDateB = document.getElementById('dateB')
+    let inputTimeB = document.getElementById('timeB')
+
+    worksInterval = 'С: ' + inputDateA.value + ' ' + inputTimeA.value + '   ' + 'ПО: ' + inputDateB.value + ' ' + inputTimeB.value
+    console.log(worksInterval)
+
+    let selectTypeOfWorks = document.getElementById('selectTypeOfWorks')
+    let typeOfWorks = selectTypeOfWorks.value
+
+    console.log(typeOfWorks)
+
+    let letsGetFullText = document.getElementById('getFullText')
+
+    document.getElementById('getFullText').style = 'display: flex;'
+    letsGetFullText.innerHTML = `
+    <textarea>Здравствуйте! Дата-центр O2XYGEN свидетельствует Вам своё уважение и информирует о следующих работах:
+    
+    Тип работ:
+    ${typeOfWorks.value}
+
+    Дата и время проведения работ:
+    ${worksInterval}
+
+    Предоставляемая услуга:
+    ${selectService.value}, Точка А: ${inputLINKa.value}${inputVOLSa.value}, Точка Б: ${inputLINKb.value}${inputVOLSb.value}
+    </textarea>
+    `
+}
